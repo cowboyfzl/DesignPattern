e60c1cd7a587fa3d115f7adb06c177f6d8d8dc13
fafaxiao <12456715@qq.com>
2019-05-08 18:05:56 +0800

    调整类名

---
+++ DesignPattern/适配器/PS2.h
@@ -0,0 +1,17 @@
+//
+//  ClassPS2.h
+//  DesignPattern
+//
+//  Created by mac on 2019/5/8.
+//  Copyright © 2019 法正磊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol PS2Protocol <NSObject>
+- (void)ps2;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DesignPattern/适配器/USB.h
@@ -0,0 +1,17 @@
+//
+//  ClassUSB.h
+//  DesignPattern
+//
+//  Created by mac on 2019/5/8.
+//  Copyright © 2019 法正磊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol USBProtocol <NSObject>
+- (void)usb;
+@end
+
+NS_ASSUME_NONNULL_END
